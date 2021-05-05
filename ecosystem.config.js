3b37f5cc7bf2aa4c2460c6b7d820b9675afdb6aa
jaffleman <jaffleman@gmail.com>
2021-05-05 21:23:40 +0200

    user freebox, path freebox

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,11 +7,11 @@ module.exports = {
   
     deploy : {
       production : {
-        user : 'jaffleman',
+        user : 'freebox',
         host : '192.168.0.29',
         ref  : 'origin/main',
         repo : 'https://github.com/jaffleman/MapTDP-mongodb.git',
-        path : '/home/jaffleman/MapTDP-mongodb',
+        path : '/home/freebox/MapTDP-mongodb',
         'pre-deploy-local': '',
         'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
         'pre-setup': ''
